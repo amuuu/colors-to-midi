d490a4604bca5fc54b25845c19c5fd78e0beedb4
Amirreza Amouie <aamouie01@gmail.com>
2020-02-26 06:05:42 -0800

    clean up thread file code

--- signalthread.py
+++ signalthread.py
@@ -12,18 +12,14 @@ class midi_signal_thread(threading.Thread):
 
     def run(self):
         try:
-            # print("inside a thread")
-            note_on = [0x90, self.value, 112] # channel 1, middle C, velocity 112
+            note_on = [0x90, self.value, 112]
             note_off = [0x80, 60, 0]
             self.midiout.send_message(note_on)
-            # print(value)
             time.sleep(2)
             self.midiout.send_message(note_off)
             time.sleep(0.2)
         finally:
-            # print("done")
             self._stop_event.set()
-            pass
     
     def get_id(self): 
         # returns id of the respective thread 
